5e050c43684a822ca5bddfead695d91cf6fc6c0a
LanDinh <admin+sourcetree@khaleesi.ninja>
2020-10-14 01:29:17 +0200

    I just want to see if this makes tests visible to Travis :(

--- scripts/backend_run.sh
+++ scripts/backend_run.sh
@@ -7,7 +7,7 @@ trap 'err=1' ERR
 coverage run "backend/${BACKEND_PROJECT}/manage.py" test
 
 cd "backend/${BACKEND_PROJECT}/"
-./manage.py test
+python -m manage test
 NO_SOURCE="__pycache__ .mypy_cache configuration"
 DIRS=""
 
