517d01b435dcaaf71920f286f38595331eafefc6
LanDinh <admin+sourcetree@khaleesi.ninja>
2020-10-15 16:59:05 +0200

    Add translate package.

---
+++ backend/translation/translate/apps.py
@@ -0,0 +1,7 @@
+"""The translate config."""
+from django.apps import AppConfig
+
+
+class TranslateConfig(AppConfig):
+  """The translate config."""
+  name = 'translate'
---
+++ backend/translation/translate/translation.py
@@ -0,0 +1,6 @@
+"""Handle translations."""
+
+
+def translate(string: str) -> str :
+  """Translate the given string."""
+  return string
