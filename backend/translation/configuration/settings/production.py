be1ccdfc82ba2b348f35e7c9ad9f2ea66dc8f219
LanDinh <admin+sourcetree@khaleesi.ninja>
2020-10-16 00:05:19 +0200

    Accidentally added production setting without prod environment.

--- backend/translation/configuration/settings/production.py
+++
@@ -1,9 +0,0 @@
-"""Production settings for khaleesi.ninja."""
-
-# pylint: disable=wildcard-import,unused-wildcard-import
-
-# khaleesi.ninja
-from base.settings.production import *
-from configuration.settings._base import *
-
-INSTALLED_APPS = BASE_INSTALLED_APPS + KHALEESI_INSTALLED_APPS
