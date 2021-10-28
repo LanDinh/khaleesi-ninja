b86b626d1598350ec034049d8dddc2592bd84b3a
LanDinh <coding+sourcetree@khaleesi.ninja>
2021-10-28 23:55:03 +0200

    Add eslint to frontend.

--- frontend/core/src/App.tsx
+++ frontend/core/src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
+
+/**
+ * Renders the app.
+ * @return {JSX.Element}
+ * @constructor
+ */
 function App() {
   return (
     <div className="App">
@@ -20,7 +26,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
