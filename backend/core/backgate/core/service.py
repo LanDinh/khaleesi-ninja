29fe012071aab9b8111b7ad149309702c4e5a294
LanDinh <coding+sourcetree@khaleesi.ninja>
2021-12-02 06:59:44 +0100

    Even more test fixes

--- backend/core/backgate/core/service.py
+++ backend/core/backgate/core/service.py
@@ -5,7 +5,7 @@ import grpc
 
 # khaleesi-ninja.
 from khaleesi.proto.core_backgate_pb2 import DESCRIPTOR, SayHelloRequest, SayHelloResponse
-from khaleesi.proto.core_backgate_pb2_grpc import GateKeeperServicer, add_GateKeeperServicer_to_server
+from khaleesi.proto.core_backgate_pb2_grpc import GateKeeperServicer, add_GateKeeperServicer_to_server  # pylint: disable=line-too-long
 
 
 class Service(GateKeeperServicer):
@@ -20,6 +20,6 @@ def register_handler(server: grpc.Server) -> None :
   """Register the handler."""
   add_GateKeeperServicer_to_server(Service(), server)  # type: ignore[no-untyped-call]
 
-full_name = DESCRIPTOR.services_by_name['Service'].full_name
+full_name = DESCRIPTOR.services_by_name['GateKeeper'].full_name
 
 service_configuration = (full_name, register_handler)
