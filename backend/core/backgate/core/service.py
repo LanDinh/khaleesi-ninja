3ccfce1f4e89e6f86cb8e006a45500ba5e254f6a
LanDinh <coding+sourcetree@khaleesi.ninja>
2021-12-02 06:32:18 +0100

    Adjust proto service naming.

--- backend/core/backgate/core/service.py
+++ backend/core/backgate/core/service.py
@@ -5,10 +5,10 @@ import grpc
 
 # khaleesi-ninja.
 from khaleesi.proto.core_backgate_pb2 import DESCRIPTOR, SayHelloRequest, SayHelloResponse
-from khaleesi.proto.core_backgate_pb2_grpc import ServiceServicer, add_ServiceServicer_to_server
+from khaleesi.proto.core_backgate_pb2_grpc import GateKeeperServicer, add_GateKeeperServicer_to_server
 
 
-class Service(ServiceServicer):
+class Service(GateKeeperServicer):
   """Core Backgate Service"""
 
   def SayHello(self, request: SayHelloRequest, _: grpc.ServicerContext) -> SayHelloResponse :  # pylint: disable=invalid-name,no-self-use
@@ -18,7 +18,7 @@ class Service(ServiceServicer):
 
 def register_handler(server: grpc.Server) -> None :
   """Register the handler."""
-  add_ServiceServicer_to_server(Service(), server)  # type: ignore[no-untyped-call]
+  add_GateKeeperServicer_to_server(Service(), server)  # type: ignore[no-untyped-call]
 
 full_name = DESCRIPTOR.services_by_name['Service'].full_name
 
