7d148483f99fd8087e4179c5dcb107e0809a1d93
LanDinh <coding+sourcetree@khaleesi.ninja>
2021-12-10 00:13:32 +0100

    Fix tests.

--- backend/core/backgate/settings.py
+++ backend/core/backgate/settings.py
@@ -2,3 +2,6 @@
 
 # khaleesi-ninja.
 from core.backgate_settings import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+
+KHALEESI_NINJA = KHALEESI_NINJA  # type: ignore[name-defined]  # pylint: disable=self-assigning-variable
