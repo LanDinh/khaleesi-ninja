7c2d3f7580cc0c7ef063c1145fd021d69478e01c
LanDinh <coding+sourcetree@khaleesi.ninja>
2021-12-09 21:08:40 +0100

    Kubegres improve initialization.

---
+++ kubernetes/khaleesi-ninja-environment/data/kubegres-initialize-postgres.sql
@@ -0,0 +1,29 @@
+CREATE FUNCTION pg_temp.create_users_and_database(
+    superUser text, superUserPassword text,
+    writeUser text, writePassword text,
+    readUser text, readPassword text,
+    database text
+) RETURNS VOID AS $$
+BEGIN
+  CREATE EXTENSION IF NOT EXISTS dblink;
+
+  EXECUTE FORMAT('CREATE USER %I PASSWORD %L', writeUser, writePassword);
+  EXECUTE FORMAT('CREATE USER %I PASSWORD %L', readUser, readPassword);
+
+  PERFORM DBLINK_EXEC(
+      FORMAT('host=127.0.0.1 dbname=postgres user=%I password=%L', superUser, superUserPassword),
+      FORMAT('CREATE DATABASE %I', database));
+
+EXCEPTION
+  WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+  WHEN duplicate_database THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$ LANGUAGE plpgsql;
+
+
+SELECT pg_temp.create_users_and_database(
+    :'superUser', :'superUserPassword',
+    :'writeUser', :'writePassword',
+    :'readUser', :'readPassword',
+    :'database'
+);
---
+++ kubernetes/khaleesi-ninja-environment/data/kubegres-perform-initialization.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# set -x          # Print subcommands to terminal.
+set -e          # Fail if any command fails
+set -u          # Define variables before usage
+set -o pipefail # Make pipes fail
+
+
+# Colors.
+magenta='\033[0;35m'
+green='\033[0;32m'
+clear_color='\033[0m'
+
+
+if [[ "${CI:-false}" == "true" ]]; then
+  export TERM=xterm-color
+fi
+
+
+echo -e "${magenta}Creating users and database...${clear_color}"
+psql -a -f /config/initialize-postgres.sql \
+    -d postgres\
+    -v superUser="${KHALEESI_DATABASE_SUPERUSER}" -v superUserPassword="${KHALEESI_DATABASE_SUPERUSER_PASSWORD}"\
+    -v writeUser="${KHALEESI_DATABASE_WRITE_USER}" -v writePassword="${KHALEESI_DATABASE_WRITE_PASSWORD}"\
+    -v readUser="${KHALEESI_DATABASE_READ_USER}" -v readPassword="${KHALEESI_DATABASE_READ_PASSWORD}"\
+    -v database="${KHALEESI_DATABASE_NAME}"
+
+
+echo -e "${magenta}Asserting permissions...${clear_color}"
+psql -a -f /config/initialize-database.sql \
+    -d "${KHALEESI_DATABASE_NAME}"\
+    -v writeUser="${KHALEESI_DATABASE_WRITE_USER}" -v readUser="${KHALEESI_DATABASE_READ_USER}"\
+    -v database="${KHALEESI_DATABASE_NAME}"
+
+echo -e "${green}DONE! :D${clear_color}"
