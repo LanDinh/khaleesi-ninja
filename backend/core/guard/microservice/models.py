eb177e963f644887d51a2ade82f563c13e922217
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-01-01 18:03:20 +0100

    Fix tests that mysteriously started failing.....

--- backend/core/guard/microservice/models.py
+++ backend/core/guard/microservice/models.py
@@ -1,8 +1,11 @@
 """core-guard models."""
 
+# Django.
 from django.db import models
 
 
 class TestModel(models.Model):
   """Test model."""
   text = models.CharField(max_length = 200)
+
+  objects = models.Manager()
