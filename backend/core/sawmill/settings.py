78a0b079a723dbbbe1118b528a3af31b7d3aef12
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-01-02 22:55:34 +0100

    Simplify settings.

--- backend/core/sawmill/settings.py
+++ backend/core/sawmill/settings.py
@@ -1,8 +1,9 @@
 """Settings for core-sawmill."""
 
+# pylint: disable=undefined-variable
+
 # khaleesi-ninja.
 from khaleesi.core.settings.khaleesi import  *  # pylint: disable=wildcard-import,unused-wildcard-import
 
-
-INSTALLED_APPS.append('microservice')  # pylint: disable=undefined-variable
-KHALEESI_NINJA['GRPC_HANDLERS'].append('microservice.service')  # pylint: disable=undefined-variable
+INSTALLED_APPS.append('microservice')
+KHALEESI_NINJA['GRPC_HANDLERS'].append('microservice.service')
