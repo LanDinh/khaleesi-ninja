4833c9ed78e718a0447e49cbffbd7293d6f2a7bf
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-01-07 23:54:49 +0100

    Fix tests for real.

--- backend/core/backgate/tests/test_khaleesi/test_core/test_metrics/test_health.py
+++ backend/core/backgate/tests/test_khaleesi/test_core/test_metrics/test_health.py
@@ -4,11 +4,13 @@
 from khaleesi.core.metrics import HEALTH
 from khaleesi.core.metrics.health import HealthMetricType
 from khaleesi.core.test_util import SimpleTestCase
-from tests.test_khaleesi.test_core.test_metrics.test_util import EnumMetricTestHelper
+from tests.test_khaleesi.test_core.test_metrics.test_util import EnumMetricTestMixin
 
 
-class HealthMetricTestCase(SimpleTestCase, EnumMetricTestHelper):
+class HealthMetricTestCase(SimpleTestCase, EnumMetricTestMixin[HealthMetricType]):
   """Test the server health metric."""
+
+  metric = HEALTH
 
   def test_set_healthy(self) -> None :
     """Test setting to healthy."""
@@ -22,4 +24,4 @@ class HealthMetricTestCase(SimpleTestCase, EnumMetricTestHelper):
     # Execute test.
     HEALTH.set_terminating()
     # Assert result.
-    self.assert_value(value = HealthMetricType.terminating)
+    self.assert_enum_metric_value(value = HealthMetricType.terminating)
--- backend/core/backgate/tests/test_khaleesi/test_core/test_metrics/test_util.py
+++ backend/core/backgate/tests/test_khaleesi/test_core/test_metrics/test_util.py
@@ -2,33 +2,36 @@
 
 # Python.
 from enum import Enum
-from typing import TypeVar, Callable
+from typing import TypeVar, Generic
 
 # khaleesi.ninja.
 from khaleesi.core.metrics import HEALTH
 from khaleesi.core.metrics.health import HealthMetricType
+from khaleesi.core.metrics.util import EnumMetric
 from khaleesi.core.test_util import SimpleTestCase
 
 
 T = TypeVar('T', bound = Enum)  # pylint: disable=invalid-name
 
 
-class EnumMetricTestHelper:
+class EnumMetricTestMixin(Generic[T]):
   """Helper methods for test classes for enum metrics."""
 
-  assertEqual: Callable[ [int, int], None ]
+  metric: EnumMetric[T]
 
-  def assert_enum_metric_value(self, *, value: T):
+  def assert_enum_metric_value(self, *, value: T) -> None :
     """Assert the metric values."""
     total = 0
     for value_counter in type(value):
-      total += HEALTH.get_value(value = value_counter)
-    self.assertEqual(1, HEALTH.get_value(value = value))
-    self.assertEqual(1, total)
+      total += self.metric.get_value(value = value_counter)
+    self.assertEqual(1, self.metric.get_value(value = value))  # type: ignore[attr-defined]  # pylint: disable=no-member
+    self.assertEqual(1, total)  # type: ignore[attr-defined]  # pylint: disable=no-member
 
 
-class EnumMetricTestCase(SimpleTestCase, EnumMetricTestHelper):
+class EnumMetricTestCase(SimpleTestCase, EnumMetricTestMixin[HealthMetricType]):
   """Test metric representing enum data."""
+
+  metric = HEALTH
 
   def test_set(self) -> None :
     """Test setting a value."""
