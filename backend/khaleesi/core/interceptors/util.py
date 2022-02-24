851cbcd37b1a9e5013dc9bd8dec4f2c02a1c5733
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-02-24 23:19:04 +0100

    Add missing file.

---
+++ backend/khaleesi/core/interceptors/util.py
@@ -0,0 +1,26 @@
+"""Interceptor utility."""
+
+# Python.
+from typing import Tuple
+
+
+class Interceptor:
+  """Interceptor utility."""
+
+  @staticmethod
+  def process_method_name(*, raw: str) -> Tuple[str, str, str, str] :
+    """Process the method name and return (service, method)."""
+    parts = raw.split('/')
+    service          = parts[1].split('.') if len(parts) > 1 else ''.split('.')
+    khaleesi_gate    = service[1] if len(service) > 1 else ''
+    khaleesi_service = service[2] if len(service) > 2 else ''
+    grpc_service     = service[3] if len(service) > 3 else ''
+    grpc_method      = parts[2]   if len(parts) > 2   else ''
+    return khaleesi_gate, khaleesi_service, grpc_service, grpc_method
+
+  @staticmethod
+  def string_or_unknown(*, value: str) -> str :
+    """Either return the value, or UNKNOWN if empty."""
+    if value:
+      return value
+    return 'UNKNOWN'
