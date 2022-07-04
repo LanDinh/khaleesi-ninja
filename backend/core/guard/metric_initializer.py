2f84887c9b9df9c15be5773a66058d66ce1d5382
LanDinh <coding+sourcetree@khaleesi.ninja>
2022-07-04 12:59:21 +0200

    Fix tests.

---
+++ backend/core/guard/metric_initializer.py
@@ -0,0 +1,8 @@
+"""Metric utility."""
+
+# khaleesi.ninja.
+from khaleesi.core.metrics.metric_initializer import BaseMetricInitializer
+
+
+class MetricInitializer(BaseMetricInitializer):
+  """Collect info for initializing metrics."""
