8ff98c212bd4e70ae17d30e2432c0c51da67fb0a
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-22 14:24:59 +0100

    Fix import... again.

--- .github/actions/load-image/index.js
+++ .github/actions/load-image/index.js
@@ -1,8 +1,8 @@
 import * as core from '@actions/core'
-import { action } from './action.js'
+import { runAction } from './action.js'
 
 
 const image = core.getInput('image')
-action.runAction(image)
+runAction(image)
     .then(() => console.log('Finished loading the image.'))
     .catch((error) => core.setFailed(error.message))
