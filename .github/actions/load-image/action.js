4ade7d1dc8619f98e480887d4075d4a6d127a5a7
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-22 15:09:38 +0100

    Try to run update pip only when necessary, and introduce commit hash to caching images.

--- .github/actions/load-image/action.js
+++ .github/actions/load-image/action.js
@@ -1,9 +1,9 @@
 import * as cache from '@actions/cache'
 import * as dockerCli from 'docker-cli-js'
 
-export async function runAction(image) {
-  const path = `/tmp/khaleesi/images/${image}`
-  const key = `khaleesi/images/${image}`
+export async function runAction(image, commit) {
+  const key = `khaleesi/${commit}/images/${image}`
+  const path = `/tmp/${key}`
   await cache.restoreCache([path], key, [])
 
   const docker = new dockerCli.Docker()
--- .github/actions/load-image/index.js
+++ .github/actions/load-image/index.js
@@ -3,6 +3,9 @@ import { runAction } from './action.js'
 
 
 const image = core.getInput('image')
-runAction(image)
+const commit = core.getInput('commit')
+
+
+runAction(image, commit)
     .then(() => console.log('Finished loading the image.'))
     .catch((error) => core.setFailed(error.message))
