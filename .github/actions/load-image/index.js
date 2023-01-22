840bc8e8bfcbaeb4090e1c92387c37fb185a9e1c
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-22 13:45:22 +0100

    Figure out how to do async stuff in actions.

--- .github/actions/load-image/index.js
+++ .github/actions/load-image/index.js
@@ -2,8 +2,14 @@ const core = require('@actions/core');
 const cache = require('@actions/cache');
 const { docker } = require('docker-cli-js');
 
-const image = core.getInput('image');
-const path = `/tmp/khaleesi/images/${image}`
-const key = `khaleesi/images/${image}`
-await cache.restoreCache([ path ], key, [])
-await docker(`load -i ${path}`)
+async function runAction() {
+  const image = core.getInput('image');
+  const path = `/tmp/khaleesi/images/${image}`
+  const key = `khaleesi/images/${image}`
+  await cache.restoreCache([path], key, [])
+  await docker(`load -i ${path}`)
+}
+
+runAction()
+    .then(() => console.log('Finished loading the image.'))
+    .catch((error) => core.setFailed(error.message))
