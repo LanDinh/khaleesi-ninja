556997a99c1f7825f2d424de015bf53f42ee712b
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-23 02:18:52 +0100

    Saving the cache obviously only works after having saved the image to disk.

--- .github/actions/cache-image/index.js
+++ .github/actions/cache-image/index.js
@@ -12,15 +12,13 @@ export async function runAction(image, commit) {
   const key = `khaleesi/${commit}/images/${image}`
   const path = `/tmp/${key}`
   const path_directory = path.split('/').slice(0, -1).join('/')
-  console.log(path)
-  console.log(path_directory)
 
   fs.mkdirSync(path_directory, { recursive: true })
 
-  await cache.saveCache([ path ], key, [])
-
   const docker = new dockerCli.Docker()
   await docker.command(`save -o ${path} ${image}`)
+
+  await cache.saveCache([ path ], key, [])
 }
 
 
