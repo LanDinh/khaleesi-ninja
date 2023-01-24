9af12e26d8662949e6298349b1cb897596c60a2a
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-25 00:02:12 +0100

    Another attempt at waiting for kubegres to start.

--- scripts/operations/setup_environment.sh
+++ scripts/operations/setup_environment.sh
@@ -37,8 +37,11 @@ fi
 echo -e "${magenta}Deploying the core gate...${clear_color}"
 helm upgrade --install "khaleesi-ninja-${environment}-core" kubernetes/khaleesi-ninja-gate \
   --values "kubernetes/configuration/gate/core.yml" \
-  --values "kubernetes/configuration/environment/${environment}.yml" \
-  --wait --timeout 5m
+  --values "kubernetes/configuration/environment/${environment}.yml"
+
+echo -e "${magenta}Waiting for the environment to be ready...${clear_color}"
+sleep 5 # We need to wait for the kubegres pod to be created, else the kubectl wait command will fail.
+kubectl -n "khaleesi-ninja-${environment}" wait pod -l job!=true --for condition=ready --timeout 5m
 
 
 echo -e "${green}DONE! :D${clear_color}"
