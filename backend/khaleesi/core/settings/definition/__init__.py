7709f79424f5ffdfc07e56f2841663ba04140449
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-28 19:04:06 +0100

    Make pylint happy.

--- backend/khaleesi/core/settings/definition/__init__.py
+++ backend/khaleesi/core/settings/definition/__init__.py
@@ -1,14 +1,16 @@
 """Definition import helper."""
 
-from .base import KhaleesiNinjaSettings as KhaleesiNinjaSettings  # pylint: disable=useless-import-alias
+# pylint: disable=useless-import-alias
+
+from .base import KhaleesiNinjaSettings as KhaleesiNinjaSettings
 from .batch import Batch as Batch
-from .grpc import (  # pylint: disable=useless-import-alias
+from .grpc import (
   Grpc as Grpc,
   GrpcInterceptors as GrpcInterceptors,
   GrpcServerInterceptor as GrpcServerInterceptor,
   GrpcServerMethodNames as GrpcServerMethodNames,
   GrpcEventMethodNames as GrpcEventMethodNames,
 )
-from .metadata import Metadata as Metadata, ServiceType as ServiceType  # pylint: disable=useless-import-alias
-from .monitoring import Monitoring as Monitoring  # pylint: disable=useless-import-alias
-from .startup import Startup as Startup, MigrationsBeforeServerStart as MigrationsBeforeServerStart  # pylint: disable=useless-import-alias
+from .metadata import Metadata as Metadata, ServiceType as ServiceType
+from .monitoring import Monitoring as Monitoring
+from .startup import Startup as Startup, MigrationsBeforeServerStart as MigrationsBeforeServerStart
