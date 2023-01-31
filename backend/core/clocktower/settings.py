a73161149de4cc53664bb2721a3b3843938b10e5
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-01-31 21:54:36 +0100

    Fix test failure.

--- backend/core/clocktower/settings.py
+++ backend/core/clocktower/settings.py
@@ -6,4 +6,4 @@
 from khaleesi.core.settings.khaleesi import  *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 
-KHALEESI_NINJA['GRPC']['HANDLERS'].append('microservice.service')
+KHALEESI_NINJA['GRPC']['HANDLERS'].append('microservice.service.bellringer')
