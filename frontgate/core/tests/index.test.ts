d5ca6b8398b9a24e62b1a70e898ed777cbcb34e1
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-06-07 11:51:33 +0200

    Add minimal frontend test to make build pass.

---
+++ frontgate/core/tests/index.test.ts
@@ -0,0 +1,7 @@
+import {describe, expect, test} from '@jest/globals'
+
+describe('initial test', () => {
+  test('that passes', () => {
+    expect(42).toBe(42)
+  })
+})
--- frontgate/execute_develop.sh
+++ frontgate/execute_develop.sh
@@ -12,18 +12,18 @@ command=${1}
 test() {
   return_code=0
 
-  #echo "React tests..."
-  #if ! npm run test; then
-  #  return_code=1
-  #fi
+  echo "React tests..."
+  if ! npm run test; then
+    return_code=1
+  fi
 
-  #echo "Copy coverage reports..."
-  #if ! mkdir -p /data/; then
-  #  return_code=1
-  #fi
-  #if ! cp -a coverage "/data/coverage"; then
-  #  return_code=1
-  #fi
+  echo "Copy coverage reports..."
+  if ! mkdir -p /data/; then
+    return_code=1
+  fi
+  if ! cp -a coverage "/data/coverage"; then
+    return_code=1
+  fi
 
   echo "Eslint..."
   if ! npm run lint; then
