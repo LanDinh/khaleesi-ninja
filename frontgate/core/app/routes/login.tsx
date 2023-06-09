16dc37400b87f62ac2ccca8c3c32046dcb207d6e
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-06-09 15:01:31 +0200

    Add cookie session.

---
+++ frontgate/core/app/routes/login.tsx
@@ -0,0 +1,47 @@
+import type { V2_MetaFunction } from '@remix-run/node'
+import type { ActionArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { Form } from '@remix-run/react'
+import { createUserSession } from '../khaleesi/core/auth'
+
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: 'khaleesi.ninja | Login' },
+    { name: 'description', content: 'Khaleesi\'s Dragonpit: Identify yourself!' },
+  ]
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData()
+  const user = form.get('user')
+
+  console.log('before validation')
+
+  if ('string' !== typeof user) {
+    return json({ fieldErrors: null, fields: null, formError: 'wrong type' }, { status: 400 })
+  }
+
+  console.log('validation passed')
+  return createUserSession(user, '/')
+}
+
+
+export default function LoginRoute() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <section><Form method="post">
+        <label>
+          <input type="radio" name="user" value="user" defaultChecked />
+          user
+        </label>
+        <label>
+          <input type="radio" name="user" value="admin" />
+          admin
+        </label>
+        <button type="submit" className="button">Login</button>
+      </Form></section>
+    </div>
+  )
+}
---
+++ frontgate/khaleesi/app/khaleesi/core/auth.ts
@@ -0,0 +1,27 @@
+import {
+  createCookieSessionStorage,
+  redirect,
+} from '@remix-run/node'
+
+
+const sessionSecret = 'shouldBeSetByBackend'
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name    : 'khaleesi_session',
+    //domain  : process.env.KHALEESI_DOMAIN,
+    secure  : true,
+    secrets : [sessionSecret],
+    sameSite: 'lax',
+    path    : '/',
+    maxAge  : 60 * 60 * 24 * 30,  // 30d.
+    httpOnly: true,
+  },
+})
+
+export async function createUserSession(sessionId: string, redirectTo: string) {
+  const session = await storage.getSession()
+  session.set('sessionId', sessionId)
+  console.log('session set')
+  return redirect(redirectTo, { headers: { 'Set-Cookie': await storage.commitSession(session) } })
+}
