a0383e15a2485ec6c52d265f74eb1f3f51e50001
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-06-11 17:28:56 +0200

    Add capability to call all cleanup requests on sawmill.

--- backend/core/clocktower/microservice/actuator/core/sawmill.py
+++ backend/core/clocktower/microservice/actuator/core/sawmill.py
@@ -12,11 +12,31 @@ from khaleesi.proto.core_sawmill_pb2_grpc import SawyerStub
 STUB = SawyerStub(CHANNEL_MANAGER.get_channel(gate = 'core', service = 'sawmill'))  # type: ignore[no-untyped-call]  # pylint: disable=line-too-long
 
 
+def cleanup_events(request: JobCleanupRequest) -> EmptyResponse :
+  """Cleanup requests."""
+  return cast(EmptyResponse, STUB.CleanupEvents(request))
+
 def cleanup_requests(request: JobCleanupRequest) -> EmptyResponse :
   """Cleanup requests."""
   return cast(EmptyResponse, STUB.CleanupRequests(request))
 
+def cleanup_errors(request: JobCleanupRequest) -> EmptyResponse :
+  """Cleanup requests."""
+  return cast(EmptyResponse, STUB.CleanupErrors(request))
+
+def cleanup_backgate_requests(request: JobCleanupRequest) -> EmptyResponse :
+  """Cleanup requests."""
+  return cast(EmptyResponse, STUB.CleanupBackgateRequests(request))
+
+def cleanup_queries(request: JobCleanupRequest) -> EmptyResponse :
+  """Cleanup requests."""
+  return cast(EmptyResponse, STUB.CleanupQueries(request))
+
 
 SAWMILL = {
-    'cleanup-requests': cleanup_requests,
+    'cleanup-events'           : cleanup_events,
+    'cleanup-requests'         : cleanup_requests,
+    'cleanup-errors'           : cleanup_errors,
+    'cleanup-backgate-requests': cleanup_backgate_requests,
+    'cleanup-queries'          : cleanup_queries,
 }
--- backend/core/clocktower/tests/test_microservice/test_actuator/test_core/test_sawmill.py
+++ backend/core/clocktower/tests/test_microservice/test_actuator/test_core/test_sawmill.py
@@ -6,12 +6,27 @@ from unittest.mock import patch, MagicMock
 # khaleesi.ninja.
 from khaleesi.core.test_util.test_case import SimpleTestCase
 from khaleesi.proto.core_pb2 import JobCleanupRequest
-from microservice.actuator.core.sawmill import cleanup_requests
+from microservice.actuator.core.sawmill import (
+  cleanup_events,
+  cleanup_requests,
+  cleanup_errors,
+  cleanup_backgate_requests,
+  cleanup_queries,
+)
 
 
 @patch('microservice.actuator.core.sawmill.STUB')
 class SawmillJobTestCase(SimpleTestCase):
   """Test core-sawmill batch jobs."""
+
+  def test_cleanup_events(self, stub: MagicMock) -> None :
+    """Test cleaning up requests."""
+    # Prepare data.
+    request = JobCleanupRequest()
+    # Execute test.
+    cleanup_events(request)
+    # Assert result.
+    stub.CleanupEvents.assert_called_once_with(request)
 
   def test_cleanup_requests(self, stub: MagicMock) -> None :
     """Test cleaning up requests."""
@@ -21,3 +36,30 @@ class SawmillJobTestCase(SimpleTestCase):
     cleanup_requests(request)
     # Assert result.
     stub.CleanupRequests.assert_called_once_with(request)
+
+  def test_cleanup_errors(self, stub: MagicMock) -> None :
+    """Test cleaning up requests."""
+    # Prepare data.
+    request = JobCleanupRequest()
+    # Execute test.
+    cleanup_errors(request)
+    # Assert result.
+    stub.CleanupErrors.assert_called_once_with(request)
+
+  def test_cleanup_backgate_requests(self, stub: MagicMock) -> None :
+    """Test cleaning up requests."""
+    # Prepare data.
+    request = JobCleanupRequest()
+    # Execute test.
+    cleanup_backgate_requests(request)
+    # Assert result.
+    stub.CleanupBackgateRequests.assert_called_once_with(request)
+
+  def test_cleanup_queries(self, stub: MagicMock) -> None :
+    """Test cleaning up requests."""
+    # Prepare data.
+    request = JobCleanupRequest()
+    # Execute test.
+    cleanup_queries(request)
+    # Assert result.
+    stub.CleanupQueries.assert_called_once_with(request)
