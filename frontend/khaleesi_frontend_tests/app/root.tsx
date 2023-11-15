f9c9aea64ee64ac575123be468444994ab1c919a
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-11-15 19:59:28 +0100

    Fix build again.

---
+++ frontend/khaleesi_frontend_tests/app/root.tsx
@@ -0,0 +1,6 @@
+import { links, ErrorBoundary, App, handle } from './khaleesi/components/document'
+import { navigationProperties } from './khaleesi/navigation/navigationData'
+
+
+export default App
+export { links, ErrorBoundary, navigationProperties, handle }
