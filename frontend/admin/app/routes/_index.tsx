22314c61fa9ba7902d4c68515254601ccdd0bdbc
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-11-15 20:23:58 +0100

    Fix eslint.

--- frontend/admin/app/routes/_index.tsx
+++ frontend/admin/app/routes/_index.tsx
@@ -3,17 +3,17 @@ import styles from '../khaleesi/styles/index.css'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "khaleesi.ninja | New Frontend" },
-    { name: "description", content: "Khaleesi's New Frontend." },
+    { title: 'khaleesi.ninja | New Frontend' },
+    { name: 'description', content: 'Khaleesi\'s New Frontend.' },
   ]
 }
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
+  { rel: 'stylesheet', href: styles },
 ]
 
 
-export default function Index() {
+export default function Index(): JSX.Element {
   return (
     <div>New Frontend</div>
   )
