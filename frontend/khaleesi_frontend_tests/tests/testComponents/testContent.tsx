ebca67916b92caa3ad89b1a2918aaf8bd7ffb890
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-11-17 13:40:47 +0100

    Fix tests.

--- frontend/khaleesi_frontend_tests/tests/testComponents/testContent.tsx
+++ frontend/khaleesi_frontend_tests/tests/testComponents/testContent.tsx
@@ -1,19 +1,11 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import { BreadCrumbs } from '../../app/khaleesi/navigation/breadcrumb'
 import { Content } from '../../app/khaleesi/components/content'
 
 
-jest.mock('../../app/khaleesi/navigation/breadcrumb')
-
-
 test('Content gets rendered without errors.', () => {
-  // Prepare data.
-  const mockBreadCrumbs = BreadCrumbs as jest.MockedFunction<typeof BreadCrumbs>
-  mockBreadCrumbs.mockImplementation(() => <></>)
   // Execute test.
   render(<Content>TEST</Content>)
   // Assert result.
-  expect(mockBreadCrumbs).toHaveBeenCalled()
   expect(screen.getByText('TEST')).toBeInTheDocument()
 })
--- frontend/khaleesi_frontend_tests/tests/testComponents/testDocument.tsx
+++ frontend/khaleesi_frontend_tests/tests/testComponents/testDocument.tsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import { App, ErrorBoundary, links } from '../../app/khaleesi/components/document'
 import { Navigation } from '../../app/khaleesi/navigation/navigation'
+import { BreadCrumbs } from '../../app/khaleesi/navigation/breadcrumb'
 import { Content } from '../../app/khaleesi/components/content'
 import { suppressConsoleFunction } from '../util/consoleLogging'
 import { createTestingStub } from '../util/remixStub'
@@ -10,6 +11,7 @@ import { createTestingStub } from '../util/remixStub'
 const originalError = console.error.bind(console.error)
 jest.mock('../../app/khaleesi/navigation/navigation')
 jest.mock('../../app/khaleesi/components/content')
+jest.mock('../../app/khaleesi/navigation/breadcrumb')
 
 beforeAll(() => {
   window.scrollTo = jest.fn()
@@ -27,12 +29,15 @@ test('App gets rendered without errors.', () => {
   mockNavigationBar.mockImplementation(() => <></>)
   const mockContent = Content as jest.MockedFunction<typeof  Content>
   mockContent.mockImplementation(() => <></>)
+  const mockBreadCrumbs = BreadCrumbs as jest.MockedFunction<typeof BreadCrumbs>
+  mockBreadCrumbs.mockImplementation(() => <></>)
   let RemixStub = createTestingStub(() => <App title="Test App" />)
   // Execute test.
   render(<RemixStub />)
   // Assert result.
   expect(mockNavigationBar).toHaveBeenCalled()
   expect(mockContent).toHaveBeenCalled()
+  expect(mockBreadCrumbs).toHaveBeenCalled()
 })
 
 test('ErrorBoundary gets rendered without errors.', () => {
@@ -41,12 +46,15 @@ test('ErrorBoundary gets rendered without errors.', () => {
   mockNavigationBar.mockImplementation(() => <></>)
   const mockContent = Content as jest.MockedFunction<typeof  Content>
   mockContent.mockImplementation(() => <></>)
+  const mockBreadCrumbs = BreadCrumbs as jest.MockedFunction<typeof BreadCrumbs>
+  mockBreadCrumbs.mockImplementation(() => <></>)
   let RemixStub = createTestingStub(() => <ErrorBoundary title="Test App" />)
   // Execute test.
   render(<RemixStub />)
   // Assert result.
   expect(mockNavigationBar).toHaveBeenCalled()
   expect(mockContent).toHaveBeenCalled()
+  expect(mockBreadCrumbs).toHaveBeenCalled()
 })
 
 test('links contain all links.', () => {
