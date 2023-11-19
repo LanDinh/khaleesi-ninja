9468ae748f1cc31b2799aaab1a8cd9e7f4bbad12
LanDinh <coding+sourcetree@khaleesi.ninja>
2023-11-19 22:25:22 +0100

    Fix tests.

--- frontend/khaleesi_frontend_tests/app/navigationData.tsx
+++ frontend/khaleesi_frontend_tests/app/navigationData.tsx
@@ -1,11 +1,7 @@
 import type { NavigationElementProperties } from './khaleesi/navigation/navigationElement'
-import { navigationProperties as kitchen } from './routes/kitchen'
-import { navigationProperties as cookbook } from './routes/kitchen.cookbook'
 
 
-export const navigationData: NavigationElementProperties[] = [
-  { ...kitchen, children: [ cookbook ] },
-]
+export const navigationData: NavigationElementProperties[] = []
 
 export const iconLookup: { [id: string]: JSX.Element } = {
   'Test': <div>Test Icon</div>
